e1f2a95d3aaeb6be1b7d086eb8436b1e1a385c7c
Constantin <paladuta05@gmail.com>
2024-12-02 17:19:24 +0200

    ref

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
-import * as cookieParser from 'cookie-parser'
+import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use(cookieParser())
+  app.use(cookieParser());
+
   app.enableCors({
-    origin: [process.env.CLIENT_URL],
+    origin: [process.env.CLIENT_URL || 'http://localhost:3000'], // Default pentru local
     credentials: true,
-    exposedHeaders: 'set-cookie'
-  })
+    exposedHeaders: 'set-cookie',
+  });
+
+  app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+  });
 
   await app.listen(5000);
 }
