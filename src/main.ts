268a30ab9df5c9269c66ae0cc69fbc117609b8b4
Constantin <paladuta05@gmail.com>
2025-01-03 06:39:34 +0200

    ref

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,21 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import * as cookieParser from 'cookie-parser';
-import * as express from 'express';
-import { join } from 'path';
+import { NestFactory } from '@nestjs/core'
+import { AppModule } from './app.module'
+
+import * as cookieParser from 'cookie-parser'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+	const app = await NestFactory.create(AppModule)
 
-  // Middleware pentru cookie-uri
-  app.use(cookieParser());
+	app.use(cookieParser())
+	app.enableCors({
+		origin: [process.env.CLIENT_URL],
+		credentials: true,
+		methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], 
+		exposedHeaders: ['Set-Cookie'], 
+	});
+	
+	  
 
-  // Activează CORS
-  app.enableCors({
-    origin: [process.env.CLIENT_URL],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-    exposedHeaders: ['Set-Cookie'],
-  });
-
-  // Servirea folderului uploads
-  app.use('/uploads', express.static(join(__dirname, '..', 'uploads')));
-
-  await app.listen(5000, '0.0.0.0');
+	await app.listen(5000, '0.0.0.0');
 }
-bootstrap();
+bootstrap()
